00a2366f33904af39494e3667d003097c6a87bc5
Tomek Mrugalski <tomek@isc.org>
2023-02-03 17:55:30 +0100

    Fix for running tests from different dir
     - this affects deployment scenarios where ANE is imported
       into Svarog (into station/src/analog-noise-estimator dir)

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 
 import numpy
 
@@ -6,14 +7,22 @@ from analog_noise_estimator.main import estimate
 
 class MainTest(unittest.TestCase):
 
+    def get_data_dir(self):
+        """
+        Returns the directory where this py file is located. The assumption is
+        that the test npy files will be in the same location. This is
+        important, if the sources are included in other project (such as Svarog)
+        """
+        dir, _ = os.path.split(__file__)
+        return dir + os.path.sep
+
+
     def test_good_square(self):
-        img = numpy.load("tests/good.npy")
+        img = numpy.load(self.get_data_dir() + "good.npy")
         rate = estimate(img)
         self.assertLess(rate, 0.1)
 
     def test_noisy_square(self):
-        img = numpy.load("tests/noisy.npy")
+        img = numpy.load(self.get_data_dir() + "noisy.npy")
         rate = estimate(img)
         self.assertGreater(rate, 0.99)
-
-        
